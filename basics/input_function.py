c622ea5f857349192caa132d5e01f61c64d3b7ab
Muhammadjon Qodirov <qodirovskiye@gmail.com>
2025-12-27 09:57:16 +0800

    input_function.py

---
+++ basics/input_function.py
@@ -0,0 +1,7 @@
+# Input and output
+name = input("Enter your name: ")     # input() takes input from the user
+age = input("Enter your age: ")       # age is stored as a string  (note: not only age! input() always takes input as a str)
+
+print("Hello,", name)                 # prints the user's name
+print("You are", age, "years old")    # prints the user's age
+
