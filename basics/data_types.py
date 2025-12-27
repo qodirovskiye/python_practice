937d7ae4b1426cf7b8ed19b9a775eb26b31fb31c
Muhammadjon Qodirov <qodirovskiye@gmail.com>
2025-12-27 09:48:21 +0800

    data_types.py

---
+++ basics/data_types.py
@@ -0,0 +1,10 @@
+# Basic data types
+age = 18              # int
+height = 1.75         # float
+name = "John"         # string
+is_active = True      # boolean
+
+print(type(age))
+print(type(height))                                # we use the type() function to check the data type of a variable
+print(type(name))                                  
+print(type(is_active))
